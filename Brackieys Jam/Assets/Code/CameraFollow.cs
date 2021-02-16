d9ea0086d88d10c35a47666e1c62aa8cc7e0410a
Snacks1483 <79061048+Snacks1483@users.noreply.github.com>
2021-02-15 19:01:15 -0500

    Changed camera script a little and imported the new host sprite

--- Brackieys Jam/Assets/Code/CameraFollow.cs
+++ Brackieys Jam/Assets/Code/CameraFollow.cs
@@ -7,8 +7,11 @@ public class CameraFollow : MonoBehaviour
 	[SerializeField] private Transform FollowTransform;
 	[SerializeField] private float FollowSpeed;
 	[SerializeField] private Vector3 CameraOffset;
-
-	private void FixedUpdate()
+    private void Start()
+    {
+		FollowTransform = GameObject.FindGameObjectWithTag("Host").GetComponent<Transform>();
+    }
+    private void FixedUpdate()
 	{
 		Vector3 smoothedPosition = Vector3.Lerp(transform.position, FollowTransform.position + CameraOffset, FollowSpeed);
 		transform.position = smoothedPosition;
